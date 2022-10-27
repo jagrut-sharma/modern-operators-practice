40b3543ff92a4c04f3fb334f1b53149a698bf2be
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-10-27 17:24:50 +0530

    Coding challenge, Logical operators

--- script.js
+++ script.js
@@ -31,11 +31,172 @@ const restaurant = {
     },
   },
 
+  deliveryOrder: function ({
+    time: deliveryTime = '22:00',
+    starterIndex: starterMenuItem = 'Manchurian',
+    mainIndex: mainMenuItem,
+    address: deliveryAddress = 'Russia',
+  }) {
+    console.log(
+      `Hi, your order, ${this.starterMenu[starterMenuItem]}, ${this.mainMenu[mainMenuItem]} will be delivered to ${deliveryAddress} at ${deliveryTime}. Thanks!`
+    );
+  },
+
   orderPasta: function (ingA, ingB, ingC) {
     console.log(`Here is your delicious pasta with ${ingA}, ${ingB}, ${ingC}`);
   },
+
+  orderPizza: function (mainIngredient, ...restIngredient) {
+    console.log(mainIngredient);
+    console.log(restIngredient);
+  },
 };
 
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+  printGoals: function (...playersThatScored) {
+    for (let currPlayer of playersThatScored) console.log(currPlayer);
+    console.log(playersThatScored.length);
+  },
+};
+
+// Coding challenge
+
+/*
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+const [gk, ...restPlayers] = players1;
+console.log(gk);
+console.log(restPlayers);
+
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+
+const { team1, x: draw, team2 } = game.odds;
+console.log(team1, draw, team2);
+
+game.printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+game.printGoals(...game.scored);
+
+team1 < team2 && console.log(`Team-1 is more likely to win.`);
+team2 < team1 && console.log(`Team-2 is more likely to win.`);
+
+/*
+// Short Circuiting
+console.log(`-------------- OR ---------------`);
+console.log(3 || 'A String');
+console.log(0 || 'A String again'); // Will return second value
+console.log(null || undefined); // Both falsy, returns last value
+console.log(3 || 0); // Returns 3 as it short circuited
+
+console.log(null || undefined || 0 || '' || 46 || 'abc' || NaN); // 46 => truthy value => shortcircuit
+
+restaurant.numGuests = 0;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+
+const guests2 = restaurant.numGuests || 15;
+console.log(guests2);
+
+// If numGuest = 0, above condition doesn't work. Use nullish coalescing operator in that case.
+
+const guests3 = restaurant.numGuests ?? 15;
+console.log(guests3);
+
+console.log(`----------- AND -----------`);
+console.log(7 && 'ABC');
+console.log(0 && 'DEF');
+console.log(7 && 'ABC' && null && 78);
+
+// instead of multiple line function => single line can be used
+
+if (restaurant.orderPizza) {
+  restaurant.orderPizza('Tomato', 'cheese', 'Paneer');
+}
+
+restaurant.orderPizza &&
+  restaurant.orderPizza('Black-Olives', 'Cheese', 'Red Paprika'); // called in single line
+
+// ☝ use only if necessary
+
+/*
+// REST Operator ---> 1. Arrays
+
+const arr = [1, 2, ...[3, 4, 5]];
+console.log(arr);
+
+const [x, , ...y] = arr;
+console.log(x, y);
+
+console.log([...restaurant.mainMenu, ...restaurant.starterMenu]);
+
+const [pizza, , risotto, ...otherMenuItems] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherMenuItems);
+
+const { sat, ...weekDays } = restaurant.openingHours;
+console.log(sat, weekDays);
+
+// REST Operator ---> 2. Functions
+const add = function (...numbers) {
+  console.log(numbers);
+  let sum = 0;
+  for (let currNumber of numbers) sum += currNumber;
+  console.log(sum);
+};
+
+add(3, 5);
+add(1, 2, 3, 4, 5);
+add(6, 7, 8, 2, 5, 1, 3);
+
+restaurant.orderPizza('Tomato');
+restaurant.orderPizza('Tomato', 'Paneer', 'Black Olives');
+restaurant.orderPizza('Tomato', 'Cheese', 'Black Olives', 'Pineapple');
+
+/*
 // Spread operator
 const arr = [1, 2, 3];
 const newArr = [...arr, 4, 5, 6];

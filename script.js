1f119a3b96d1b4ffcc7657c8cf97579f2030af52
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-10-27 18:12:57 +0530

    logical assignment operator

--- script.js
+++ script.js
@@ -97,6 +97,39 @@ const game = {
   },
 };
 
+/*
+// Logical Assignment Operators
+
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'La Piazza',
+  owner: 'Giovanni Rossi',
+};
+
+// rest2.numGuests = rest2.numGuests || 10;
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests ||= 10;
+// rest1.numGuests ||= 30;
+
+// nullish arguments
+rest2.numGuests ??= 10;
+rest1.numGuests ??= 30;
+console.log(rest2.numGuests);
+console.log(rest1.numGuests);
+
+// AND logical assigment operator
+// rest2.owner = rest2.owner && 'Anonymous';
+// rest1.owner = rest1.owner && 'Anonymous';
+rest2.owner &&= 'Anonymous';
+rest1.owner &&= 'Anonymous';
+console.log(rest1);
+console.log(rest2);
+
 // Coding challenge
 
 /*

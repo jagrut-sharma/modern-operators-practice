c260cc4aedaa5f7ccfc4ee7b9b522bd21dac5c05
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-10-27 23:00:00 +0530

    optional chaining

--- script.js
+++ script.js
@@ -4,6 +4,20 @@
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
+const openingHours = {
+  thu: {
+    open: 12,
+    close: 22,
+  },
+  fri: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -12,24 +26,11 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  orderFood: function (starterIndex, mainIndex) {
+  orderFood(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
+  openingHours,
 
   deliveryOrder: function ({
     time: deliveryTime = '22:00',
@@ -96,6 +97,29 @@ const game = {
     console.log(playersThatScored.length);
   },
 };
+
+// if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+// if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
+
+console.log(restaurant.openingHours?.mon?.open);
+console.log(restaurant.openingHours?.fri?.open);
+
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+for (let currDay of weekDays) {
+  const open = restaurant.openingHours[currDay]?.open ?? 'Closed';
+  console.log(`Our hotel opens on ${currDay} at ${open}`);
+}
+
+// Methods
+
+console.log(restaurant.orderFood?.(0, 1) ?? 'Method does not exist');
+console.log(restaurant.orderRisotto?.(0, 1) ?? 'Method does not exist');
+
+// Arrays
+
+const users = [{ name: 'Jagrut', email: 'sample.mail@jagrut.com' }];
+console.log(users[0]?.name ?? 'Element not present');
+console.log(users[1]?.name ?? 'Element not present');
 
 /*
 // Logical Assignment Operators
